0cbbaedf04eb49bbd17e157173db088fdaef0f7d
noah <sungmin26619@gmail.com>
2023-04-19 23:11:09 +0900

    complete. can get the minimum SOP

--- QM.cpp
+++ QM.cpp
@@ -36,23 +36,45 @@ bool binMatch(string &bin1, string &bin2) {
 	return true;
 }
 
-void BackTracking(vector<int> &table, set<string> &EPIs, vector<string> &trueMT, vector<string> &PIs, int idx) {
+int getNumTrans(set<string> &EPIs, int b_len) {
+	int trans_num = 0; // 트랜지스터 개수
 
+	int Or = EPIs.size(); // EPI의 개수가 OR의 input 개수
+	if (Or > 1) // 인풋 개수는 항상 2보다 큼 
+		trans_num += (Or << 1) + 2; // (NOR(input * 2) + inverter(2) => OR)
+	
+	int Not = 0;	int And = 0;
+	for (auto &k : EPIs) {
+		int cnt_dash = 0; // '-' 대시 하나마다 AND input 하나씩 감소
+		int cnt_zero = 0; // 인버터를 위해 0이 있는 자리 계산
+		for (int i = 0; i < b_len; i++) {
+			if (k[i] == '-') cnt_dash++;
+			if (k[i] == '0') cnt_zero += (1 << i); // 0의 위치 정보를 2진법에서 10진법으로 전환
+		}
+		And = b_len - cnt_dash;
+		if (And > 1) // 인풋 개수는 항상 2보다 큼
+			trans_num += (And << 1) + 2;  // (NAND(input * 2) + inverter(2) => AND)
+		
+		Not |= cnt_zero; // 0이 있는 위치들을 합집합연산(각 인덱스별로 0이 있는지 없는지)
+	}
+	while (Not) {
+		if (Not % 2 == 1) trans_num += 2; // 0 한개마다 인버터 하나씩 추가(트랜지스터 두개씩)
+		Not >>= 1;
+	}
+	return trans_num;
 }
 
-
-// TODO : 제출할 때 삭제
-string file[10] = {"input_minterm.txt", "test1.txt", "test2.txt", "test3.txt", "test4.txt", "test5.txt", "test6.txt", "test7.txt", "test8.txt", "test9.txt"};
+string file[5] = {"input_minterm.txt", "test1.txt", "test2.txt", "test3.txt", "test4.txt"};
 
 int main(int argc, char *argv[]) {
+	ios::sync_with_stdio(0);
 	ofstream fout("result.txt");
 	ifstream fin;
 
-for (int fi = 0; fi < 6; fi++) {
+for (int fi = 0; fi < 5; fi++) {
 //	ifstream fin("input_minterm.txt");
 	if (argc == 2) fin.open(argv[1]);
 	else fin.open(file[fi]);	
-	fout << "\n-------------------\n#" << fi << " test case \n";
 
 	int b_len; // 비트 길이
 	fin >> b_len; 
@@ -95,9 +117,9 @@ for (int fi = 0; fi < 6; fi++) {
 			for (auto &k : uniq)
 				if (count(k.begin(), k.end(), '1') == i) MT[i].push_back(k); // 새로 만든 것들 다시 1개수 순으로 저장
 		}
-		for (int i = 0; i < b_len + 1; i++)
-			for (auto &k : MT[i]) fout << i << ' ' << k.binary << '\n';
-		fout << '\n';
+		//for (int i = 0; i < b_len + 1; i++)
+		//	for (auto &k : MT[i]) fout << i << ' ' << k.binary << '\n';
+		//fout << '\n';
 		uniq.clear();
 
 		if (brk) break;
@@ -107,10 +129,9 @@ for (int fi = 0; fi < 6; fi++) {
 //	for (auto &k : PIs) cout << k << '\n';
 	
 	set<string> EPIs; // Essential PIs
-	vector<int> table; // Essential PI를 찾기 위한 표. int형 정수를 통해 비트로 표현.
+	vector<int> table; // Essential PI를 찾기 위한 표. int형 정수를 통해 비트로 표현.(메모리, 성능)
 	int col = 0;
 
-	int flag = 1; // 유일한 PI를 가진 행이 하나도 없을 경우
 	for (int i = 0; i < trueMT.size(); i++) { // 열을 먼저 확인하기 위함
 		col = 0;
 		int cnt = 0; // 열에 PI가 얼마나 있는지 count
@@ -123,13 +144,12 @@ for (int fi = 0; fi < 6; fi++) {
 			}
 		}
 		if (cnt == 1) { // 열에 PI 하나 -> Essential PI
-			flag = 0;
 			EPIs.emplace(PIs[pos]); 
 			trueMT.erase(trueMT.begin()+i--); // 해당 true minterm은 필요 없어짐.
-			if (!table.empty()) {
+			if (!trueMT.empty()) {
 				for (int t = 0; t < i; t++) {
 					if (table[t] & (1 << pos)) {
-						table[t] = 0; // 방금 선택한 PI가 포함하는 범위. 해당 열 삭제
+						table.erase(table.begin() + t); // 방금 선택한 PI가 포함하는 범위. 해당 열 삭제
 						trueMT.erase(trueMT.begin()+t--); i--;
 					}
 				}
@@ -137,37 +157,78 @@ for (int fi = 0; fi < 6; fi++) {
 		}
 		else table.push_back(col);
 	}
-	if (flag) 
-		BackTracking(table, EPIs, trueMT, PIs, 0);
 
-	int trans_num = 0; // 트랜지스터 개수
-	b_len = PIs[0].length(); // 비트 길이 다시 구하기
+	while(!trueMT.empty()) { // EPI 다 못찾은 경우
+		int maxMt = 0; // 한 행당 minterm의 최대 개수
+		int maxpos = -1; // minterm이 가장 많은 행의 위치	
 
-	int Or = EPIs.size(); // EPI의 개수가 OR의 input 개수
-	if (Or > 1) // 인풋 개수는 항상 2보다 큼 
-		trans_num += (Or << 1) + 2; // (NOR(input * 2) + inverter(2) => OR)
-	
-	int Not = 0;	int And = 0;
-	for (auto &k : EPIs) {
-		fout << k << '\n';
-		int cnt_dash = 0; // '-' 대시 하나마다 AND input 하나씩 감소
-		int cnt_zero = 0; // 인버터를 위해 0이 있는 자리 계산
-		for (int i = 0; i < b_len; i++) {
-			if (k[i] == '-') cnt_dash++;
-			if (k[i] == '0') cnt_zero += (1 << i); // 0의 위치 정보를 2진법에서 10진법으로 전환
+		if (trueMT.size() < 2) {
+			if (trueMT.empty()) break; // 다 찾은 경우
+			for (int i = 0; i < PIs.size(); i++) { // 하나만 남은 경우
+				if (table[0] & (1 << i)) {
+					EPIs.emplace(PIs[i]);
+					break;
+				}
+			}
 		}
-		And = b_len - cnt_dash;
-		if (And > 1) // 인풋 개수는 항상 2보다 큼
-			trans_num += (And << 1) + 2;  // (NAND(input * 2) + inverter(2) => AND)
-		
-		Not |= cnt_zero; // 0이 있는 위치들을 합집합연산(각 인덱스별로 0이 있는지 없는지)
-	}
-	while (Not) {
-		if (Not % 2 == 1) trans_num += 2; // 0 한개마다 인버터 하나씩 추가(트랜지스터 두개씩)
-		Not >>= 1;
+
+		for (int i = 0; i < PIs.size(); i++) { // maxMT, maxpos 찾기
+			int cnt = 0;
+			for (int j = 0; j < trueMT.size(); j++) {
+				if (table[j] & (1 << i)) cnt++; // MT가 있으면 카운트
+			}
+			if (maxMt < cnt) {
+				maxMt = cnt;
+				maxpos = i;
+			}
+		}
+		EPIs.emplace(PIs[maxpos]); // 찾은 행 EPI로 저장
+		for (int i = 0; i < trueMT.size(); i++) { // 해당 PI가 커버하는 minterm들 삭제
+			if (table[i] & (1 << maxpos)) {
+				table.erase(table.begin() + i);
+				trueMT.erase(trueMT.begin()+i--);
+			}
+		}
+
+		if (trueMT.size() < 2) {
+			if (trueMT.empty()) break; // 다 찾은 경우
+			for (int i = 0; i < PIs.size(); i++) { // 하나만 남은 경우
+				if (table[0] & (1 << i)) {
+					EPIs.emplace(PIs[i]);
+					break;
+				}
+			}
+		}
+
+		if (trueMT.empty()) break; // minterm 개수가 최대인 행 접근으로 해결된 경우 
+
+		for (int i = 0; i < trueMT.size(); i++) { // 다시 한 번 유일 minterm이 있는 열 확인
+			int cnt = 0;
+			int pos = -1;
+			for (int j = 0; j < PIs.size(); j++) {
+				if (table[i] & (1 << j)) {
+					cnt++;
+					pos = i;
+				}
+			}
+			if (cnt == 1) {
+				EPIs.emplace(PIs[pos]); 
+				trueMT.erase(trueMT.begin()+i--); // 해당 true minterm은 필요 없어짐.
+				if (!trueMT.empty()) {
+					for (int t = 0; t < trueMT.size(); t++) {
+						if (table[t] & (1 << pos)) {
+							table[t] = 0; // 방금 선택한 PI가 포함하는 범위. 해당 열 삭제
+							trueMT.erase(trueMT.begin()+t--); i--;
+						}
+					}
+				}
+			}
+		}
 	}
 	
-	fout << "\nCost(# of transistors): " << trans_num;
+	for (auto &k : EPIs)
+			fout << k << '\n';
+	fout << "\nCost(# of transistors): " << getNumTrans(EPIs, PIs[0].length());
 
 	fin.close();
 }
